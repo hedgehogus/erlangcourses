ebf03135ac079368dfe60b49dd2986fee8e7bd96
hedgehogus <research162@gmail.com>
2017-03-17 13:30:42 +0200

    cut pieces

--- Week_1/recursion.erl
+++ Week_1/recursion.erl
@@ -1,5 +1,5 @@
 - module(recursion).
-- export([fac/1, fibo/1]).
+- export([fac/1, fibo/1, piec/1, piecsq/1]).
 
 fac(0) ->
 1;
@@ -8,11 +8,17 @@ fac (N-1)*N.
 
 fibo(0) ->
 0;
-
 fibo(1) ->
 1;
-
 fibo (N)  when N>0->
 fibo (N-1)+fibo(N-2).
 
+piec(1) ->
+2;
+piec(N) when N>1->
+piec(N-1)+N.
 
+piecsq(1) ->
+2;
+piecsq(N) when N>1->
+piecsq(N-1)*2.
