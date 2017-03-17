bb080ead6e14d7307ef8ea4d919e16d38622b6f2
hedgehogus <research162@gmail.com>
2017-03-17 04:34:24 +0200

    fibonacci

--- Week_1/recursion.erl
+++ Week_1/recursion.erl
@@ -1,10 +1,18 @@
 - module(recursion).
-- export([fac/1]).
+- export([fac/1, fibo/1]).
 
 fac(0) ->
 1;
-
 fac (N) when N>0 ->
-fac (N-1)*N;
+fac (N-1)*N.
+
+fibo(0) ->
+0;
+
+fibo(1) ->
+1;
+
+fibo (N)  when N>0->
+fibo (N-1)+fibo(N-2).
 
 
