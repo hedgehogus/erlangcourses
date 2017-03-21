422f4de61f11ff491bd9b1be1fba7b3a06b32807
hedgehogus <research162@gmail.com>
2017-03-22 00:51:44 +0200

    second variant of perfect numbers

--- Week_1/tail_recursion.erl
+++ Week_1/tail_recursion.erl
@@ -1,5 +1,5 @@
 - module(tail_recursion).
-- export([fac/1, loop/1, fib/1, perf/1]).
+- export([fac/1, loop/1, fib/1, perf/1, perfect/3]).
 
 fac(N) -> fac(N,1).
 
@@ -10,24 +10,28 @@ fac (N-1,P*N).
 loop(N) when N>0 ->
 io:format("~p~n", [N]),
 loop(N-1);
-loop(N) -> 
+loop(_N) -> 
 io:format("bye~n").
 
-fib(N) -> fib(N-2,0,1).
+fib(N) -> fib(N,0,1).
 
-fib(0,P,S) ->
-P+S;
+fib(0,P,_S) -> P;
 fib (N,P,S) when N>0 ->
-fib (N-1, P+S, P).
+    fib (N-1, S, P+S).
 
 perf(N) -> perf(N,0,N).
 
 perf(0,S,A) ->
 S == A;
-perf(N,S,A) when (N>0) ->
-perf (N-1, S+divide(A,N-1), A).
+perf(N,S,A) when (A rem (N-1)) == 0 ->
+    perf (N-1, S+(N-1), A);
+perf(N,S,A) ->
+    perf (N-1, S, A).
 
-divide(X,Y) when (X - ((X div Y)*Y) == 0) -> Y;
-divide(X,Y) -> 0. 
+perfect(N,N,S) -> N==S;
+perfect(N,M,S) when N rem M == 0 ->
+    perfect(N,M+1,S+M);
+perfect(N,M,S) ->
+    perfect(N, M+1,S).
 
-
+perfect(N)-> perfect(N,1,0).
