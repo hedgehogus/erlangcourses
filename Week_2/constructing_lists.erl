30d74e6d0d55f23c7a3c89545fd61cb5f871e411
hedgehogus <research162@gmail.com>
2017-03-24 23:32:39 +0200

    sorting function

--- Week_2/constructing_lists.erl
+++ Week_2/constructing_lists.erl
@@ -1,5 +1,5 @@
 - module (constructing_lists).
-- export([double/1, even/1]).
+- export([double/1, even/1, median/1, sort/1]).
 
 % function to double the elements of a list of numbers
 double([]) -> [];
@@ -14,9 +14,31 @@ even([_|Xs]) -> even(Xs).
 
 % the median of a list of numbers
 median ([]) ->[];
+median ([X]) -> [X];
 median (Xs) ->
-    median(Xs, lists_first:lengthl(Xs)).
+    median(sort(Xs), lists_first:lengthl(Xs) div 2, lists_first:lengthl(Xs) rem 2 ).
 
-median ([X|Xs], L) when L rem 2 == 0 ->
+median ([X,Y|_], 1, D) ->
+    case D of 
+        0 -> [X,Y];
+        _ -> [Y]
+    end;    
+median ([_X|Xs], L, D)  ->
+    median(Xs, L-1, D).
+
+% function to sort a list
+sort (Xs) -> 
+    S = sortloop (Xs),
+    case Xs of
+        S -> S;
+        _ -> sort(S) end.  
+
+sortloop ([X]) -> [X];
+sortloop ([X,Y|Xs]) when X > Y ->
+    [Y| sortloop([X|Xs])];
+sortloop ([X,Y|Xs]) ->
+    [X| sortloop([Y|Xs])].
+
+
 
     
