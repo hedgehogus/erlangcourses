accd16415b380de51047a9d70684d52723c7738c
hedgehogus <research162@gmail.com>
2017-03-26 23:49:53 +0300

    remove all repeated elements except the first instance, or all repeated elements except the final instance.

--- Week_2/solving.erl
+++ Week_2/solving.erl
@@ -1,5 +1,5 @@
 - module (solving).
-- export([take/2, take1/2, split/2, nub/1]).
+- export([take/2, take1/2, split/2, nub/1, nubb/1]).
 
 
 %% takes the first N elements from a list
@@ -9,10 +9,10 @@ take(N, [X|Xs]) when N>0->
     [X| take(N-1,Xs)].
 
 take1(N,Xs) ->
-    {Front, _Back} = split(N,Xs),
+    {Front, _Back} = take2(N, Xs, []),
     Front.
 
-take2(0, L, R) ->
+take2(0, _L, R) ->
     lists:reverse(R, []);
 take2(N, [H|T], R) ->
     take2 (N-1, T, [H|R]);
@@ -29,8 +29,18 @@ split(N, [H|T], R) ->
 split(_, [], _) ->
     badarg.
 
+%  removes all the duplicate elements from a list
 nub([]) -> [];
-nub (X|Xs) ->
-    [X| nub(nub(Xs),X).
+nub ([X|Xs]) ->
+    [X| nub(nub(Xs),X)].
 
-nub ([X|Xs],Y)
+nub([], _) -> [];
+nub ([X|Xs],X) ->
+    nub(Xs,X);
+nub ([X|Xs],Y) ->
+    [X | nub (Xs, Y)].
+
+nubb([]) -> [];
+nubb (A) ->
+    [X|Xs] = lists:reverse(A, []),
+    lists:reverse([X| nub(nub(Xs),X)]).
