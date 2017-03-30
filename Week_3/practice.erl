9e05a341dac7a46c6c143e310b691f1c37f53880
hedgehogus <research162@gmail.com>
2017-03-30 23:02:04 +0300

    zip

--- Week_3/practice.erl
+++ Week_3/practice.erl
@@ -1,5 +1,5 @@
 - module(practice).
-- export([doubleAll/1, evens/1, product/1]).
+- export([doubleAll/1, evens/1, product/1, zip/2, zip_with/3,zip2/2]).
 
 map (_, []) -> [];
 map (F, [X|Xs]) ->
@@ -31,3 +31,29 @@ multiply(A,B) -> A*B.
 
 product(X) ->
     reduce(fun multiply/2, 1, X).
+
+% 1) Define a function zip/2 that “zips together” pairs of elements from two lists
+zip(_,[]) ->[];
+zip([],_) ->[]; 
+zip([X|Xs],[Y|Ys]) ->
+    [{X,Y}| zip(Xs,Ys)].
+
+% 2) Define a function zip_with/3 that “zips together” pairs of elements from two lists using the function in the first argument, like this:
+
+zip_with(_,[],_) -> [];
+zip_with(_,_,[]) -> [];
+zip_with(F,[X|Xs],[Y|Ys])->
+    [F(X,Y)|zip_with(F,Xs,Ys)].
+
+% c) Re-define the function zip_with/3 using zip and lists:map.
+zip1({F,A,B}) -> F(A,B).
+
+zip_with1(F, Xs, Ys) ->
+    A = zip(Xs, Ys),    
+    B = lists:map(fun({X,Y}) -> {F,X,Y} end, A),
+    lists:map(fun zip1/1, B).
+
+% d) Re-define zip/2 using zip_with/3.
+zip2(X, Y) ->
+    zip_with1(fun(A,B) -> {A, B} end, X, Y).
+
