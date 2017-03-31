b3f93742703a683406133e972207b3784eb0f55b
hedgehogus <research162@gmail.com>
2017-04-01 02:18:44 +0300

    takes a list of functions and composes them together

--- Week_3/result.erl
+++ Week_3/result.erl
@@ -1,5 +1,5 @@
 - module(result).
-- export([addOneToAll/1, addToAll/2,composed/1]).
+- export([addOneToAll/1, addToAll/2,composed/1, sum/1, test/1]).
 
 add(X) -> fun(Y) -> X+Y end.
 
@@ -16,7 +16,27 @@ compose(F,G) ->
     fun(X) -> G(F(X)) end.
 
 fun1(X) -> X+1.
-fun2(X) -> X*2.
+fun2(X) -> X+2.
+fun3(X) -> X+3.
 
 composed(X) ->
-    map(compose(fun fun1/1, fun fun2/1),X).
+    A = compose(fun fun1/1, fun fun2/1),
+    A(X).
+
+sum(Xs) ->
+    Add = fun(X,Y) -> X+Y end,
+    lists:foldr(Add,0, Xs).
+
+% function that takes a list of functions and composes them together
+composeList([]) -> empty_list;
+composeList([X]) -> X;
+composeList([X|Xs]) ->
+    A = composeList(Xs),
+    fun (Y) -> X(A(Y)) end.
+
+twice(F) ->
+    fun(X) ->F(F(X)) end.
+
+test(X) ->
+    A = composeList([fun fun1/1, fun fun2/1, fun fun3/1]),
+    A(X).
