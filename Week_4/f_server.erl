5fee4c1ef3276d8b16b822a61c102143da8ab477
hedgehogus <research162@gmail.com>
2017-04-06 05:52:20 +0300

    primitive server

--- Week_4/f_server.erl
+++ Week_4/f_server.erl
@@ -1,2 +1,37 @@
 - module(f_server).
-- export([]).
+- export([start/0, init/0]).
+
+allocate({[],Allocated}, _Pid) ->
+    {{[], Allocated},{error, no_frequency}};
+allocate({[Freq|Free], Allocated}, Pid) ->
+    {{Free, [{Freq,Pid}|Allocated]}, {ok, Freq}}.
+
+deallocate ({Free, Allocated}, Freq) ->
+    NewAllocated = lists:keydelete(Freq, 1, Allocated),
+    {[Freq|Free], NewAllocated}.
+
+loop (Frequencies) ->
+    receive
+    {request, Pid, allocate} -> 
+        {NewFrequancies,Reply} = allocate (Frequencies, Pid),
+        Pid ! {reply,Reply},
+        loop(NewFrequancies);
+    {request, Pid, {deallocate, Freq}} ->
+        NewFrequancies = deallocate(Frequencies, Freq),
+        Pid ! {reply, ok},
+        loop (NewFrequancies);
+    {request, Pid, stopped} ->
+        Pid ! {reply, stopped}
+    end.
+
+init() ->
+    Frequencies = {get_frequencies(), []},
+    loop(Frequencies).
+
+% hard coded
+
+get_frequencies() -> [10,11,12,13,14,15].
+
+start()->
+    Server = spawn(f_server, init, []),
+    register(serv, Server).
