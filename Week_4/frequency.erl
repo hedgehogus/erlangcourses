4a2bceb10da4e3e6887bae170c0b816c24d9bbd5
hedgehogus <research162@gmail.com>
2017-04-07 07:20:42 +0300

    assignment

--- Week_4/frequency.erl
+++ Week_4/frequency.erl
@@ -7,10 +7,15 @@
 %%   (c) Francesco Cesarini and Simon Thompson
 
 -module(frequency).
+-export([start/0,allocate/0,deallocate/1,stop/0, clear/0, test/0]).
 -export([init/0]).
 
 %% These are the start functions used to create and
 %% initialize the server.
+
+start() ->
+    register(frequency,
+	     spawn(frequency, init, [])).
 
 init() ->
   Frequencies = {get_frequencies(), []},
@@ -20,8 +25,9 @@ init() ->
 get_frequencies() -> [10,11,12,13,14,15].
 
 %% The Main Loop
-
+%% simulating the frequency server being overloaded
 loop(Frequencies) ->
+  timer:sleep(2000),
   receive
     {request, Pid, allocate} ->
       {NewFrequencies, Reply} = allocate(Frequencies, Pid),
@@ -35,14 +41,54 @@ loop(Frequencies) ->
       Pid ! {reply, stopped}
   end.
 
+%% Functional interface
+
+test() -> 
+    io:format ("message:~w~n", [allocate()]).    
+
+allocate() -> 
+    frequency ! {request, self(), allocate},   
+    receive 
+	    {reply, Reply} -> Reply
+    after 500 -> clear()
+    end.
+
+deallocate(Freq) -> 
+    frequency ! {request, self(), {deallocate, Freq}},
+    receive 
+	    {reply, Reply} -> Reply
+    after 500 -> deallocate(Freq)
+    end.
+
+stop() -> 
+    frequency ! {request, self(), stop},
+    receive 
+	    {reply, Reply} -> Reply
+    end.
+
+
 %% The Internal Help Functions used to allocate and
 %% deallocate frequencies.
 
 allocate({[], Allocated}, _Pid) ->
   {{[], Allocated}, {error, no_frequency}};
 allocate({[Freq|Free], Allocated}, Pid) ->
-  {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
+ case  lists:keymember(Pid,2,Allocated)  of
+      true -> {[Freq|Free], {error, already_allocated}};
+      false -> {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}
+    end.
 
 deallocate({Free, Allocated}, Freq) ->
-  NewAllocated=lists:keydelete(Freq, 1, Allocated),
-  {[Freq|Free],  NewAllocated}.
+  case lists:keymember(Freq, 1,Allocated) of
+        true -> NewAllocated = lists:keydelete(Freq, 1, Allocated),
+         {{[Freq|Free], NewAllocated}, ok};
+        false -> {{[Free],Allocated}, not_currently_using}
+    end.
+
+% flushing the mailbox
+clear() ->
+  receive 
+    Msg -> io:format ("clear message:~w~n", [Msg]),
+    clear()
+    after 0 -> ok
+  end.
