14784a845b159b5037e90ba155fd8be3abcb0516
hedgehogus <research162@gmail.com>
2017-04-10 07:36:04 +0300

    normal and abnormal terminating, spawn and link processes

---
+++ Week_5/first.erl
@@ -0,0 +1,50 @@
+- module(first).
+- export([start/0, start2/0]).
+- export([loop/0, loop2/0, loop3/0, loop4/0]).
+
+start() ->
+    register(first,spawn(?MODULE, loop4, [])).
+
+% spawn and link process
+start2() ->
+    register(first,spawn_link(?MODULE, loop4, [])).
+
+% never terminates
+loop() -> 
+    receive
+        {msg, M} ->
+            io:format("ack~n"),
+            loop();
+        _Msg ->
+            loop()
+    end.
+
+% terminates if message does not acknowleged
+loop2() -> 
+    receive
+        {msg, M} ->
+            io:format("ack~n"),
+            loop2();
+        _Msg ->
+            ok
+    end.
+
+% terminates in both cases. Terminates normally
+loop3() -> 
+    receive
+        {msg, M} ->
+            io:format("ack~n");     
+        _Msg ->
+            ok
+    end.
+
+% processes can fail. Terminates abnormally
+loop4() -> 
+    receive
+        {msg, M} ->
+            N = M+1,
+            io:format("~b~n", [N]),
+            loop4();     
+        _Msg ->
+            loop4()
+    end.
