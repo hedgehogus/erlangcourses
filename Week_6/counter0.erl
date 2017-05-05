53cad3972225718e4c36416c976a16c3b488887d
hedgehogus <research162@gmail.com>
2017-05-05 18:20:56 +0300

    generics

---
+++ Week_6/counter0.erl
@@ -0,0 +1,25 @@
+- module(counter0).
+- export([start/0, loop/1, tick/1, read/0]).
+
+start() ->
+    register(counter0, spawn(counter0, loop, [0])).
+
+tick(N) -> rpc({tick, N}).
+read() -> rpc(read).
+
+rpc(Query) ->
+    Tag = make_ref(),
+    counter0 ! {self(), Tag, Query},
+    receive
+        {Tag, Reply} -> Reply
+    end.
+
+loop(State) -> 
+    receive
+        {From, Tag, {tick, N}} ->
+            From ! {Tag, ack},
+            loop(State + N);
+        {From, Tag, read} ->
+            From ! {Tag, State},
+            loop(State)
+    end.
