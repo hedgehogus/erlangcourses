b3e8598cf6e001ea3c76d404435ced5efbde38e5
hedgehogus <research162@gmail.com>
2017-05-06 18:51:08 +0300

    init & loop

--- Week_6/esp.erl
+++ Week_6/esp.erl
@@ -1,5 +1,6 @@
 - module (esp).
 - export([start_link/0, count/0, echo/1, reset/1, stop/0]).
+- export([init/0]).
 
 start_link() ->
     register(?MODULE, spawn_link(?MODULE, init,[])).
@@ -23,3 +24,22 @@ reset(N) ->
 stop() ->
     ?MODULE ! {stop, self()},
     ok.
+
+
+init() ->
+    State = 0,
+    loop(State).
+
+loop(N) ->
+    receive
+        {count, From} ->
+            From ! N,
+            loop(N);
+        {{reset, X}, _From} ->
+            loop(X);
+        {stop, _From} ->
+            ok;
+        {Msg, From} ->
+            From ! Msg,
+            loop(N+1)
+    end.
